ce08b4e74938542a60ea675ebed972903130b248
Aelita4 <kontakt@mikorosa.pl>
2023-06-24 22:05:38 +0200

    Login form

--- app/src/main/java/pl/mikorosa/webco/MainActivity.kt
+++ app/src/main/java/pl/mikorosa/webco/MainActivity.kt
@@ -2,10 +2,25 @@ package pl.mikorosa.webco
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.loginButton)
+
+        button.setOnClickListener {
+            val usernameField: EditText = findViewById(R.id.fieldUsername)
+            val passwordField: EditText = findViewById(R.id.fieldPassword)
+
+            val username = usernameField.getText().toString()
+            val password = passwordField.getText().toString()
+
+            
+        }
+
     }
 }
